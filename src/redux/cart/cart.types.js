a91e89c8ae760dcadbfed7834afeea7a15151268
Diego <diego.caudillo@uabc.edu.mx>
2021-02-27 22:08:00 -0800

    added addItem and removeItem functionality to checkout item component

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,7 +1,8 @@
 const CartActionsTypes = {
    TOOGLE_CART_HIDDEN: 'TOOGLE_CART_HIDDEN',
    ADD_ITEM: 'ADD_ITEM',
-   REMOVE_ITEM: 'REMOVE_ITEM'
+   REMOTE_ITEM: 'REMOTE_ITEM',
+   REMOVE_ITEM_FROM_CART: 'REMOVE_ITEM_FROM_CART'
 }
 
 export default CartActionsTypes;
