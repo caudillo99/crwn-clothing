db7dc4e99b0d80d172743f6ff939e80220673e9f
Diego Caudillo <dcaudillo@bortom.io>
2021-07-12 21:31:18 -0700

    Setting up StripeJS for payments (test mode). Changing some Redux code to Context API

---
+++ client/src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,102 @@
+import React, { Component } from "react";
+
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+
+import "./sign-up.styles.scss";
+import { signUpStart } from "../../redux/user/user.actions";
+import { connect } from "react-redux";
+
+class SignUp extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      displayName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  /* 
+   This syntax is experimental and not standarized yet.
+   */
+
+  async handleSubmit(e) {
+    e.preventDefault();
+
+    const { displayName, email, password, confirmPassword } = this.state;
+    const { signUpStart } = this.props;
+    console.log(this.state);
+    if (password !== confirmPassword) {
+      alert("Password don't match");
+      return;
+    }
+    signUpStart(displayName, email, password);
+    this.setState({
+      displayName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    });
+  }
+
+  // eslint-disable-next-line no-undef
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    const { displayName, email, password, confirmPassword } = this.state;
+    return (
+      <div className="sign-up">
+        <h2 className="title">I do not have a account</h2>
+        <span> Sign up with your email and password </span>
+        <form className="sign-up-form" onSubmit={this.handleSubmit}>
+          <FormInput
+            type="text"
+            name="displayName"
+            value={displayName}
+            onChange={this.handleChange}
+            label="Display Name"
+            required
+          />
+          <FormInput
+            type="email"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+            label="Email"
+            required
+          />
+          <FormInput
+            type="password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+            label="Password"
+            required
+          />
+          <FormInput
+            type="password"
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={this.handleChange}
+            label="Confirm Password"
+            required
+          />
+          <CustomButton type="submit">SIGN UP</CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+const mapDispatchToProps = (dispatch) => ({
+  signUpStart: (displayName, email, password) =>
+    dispatch(signUpStart({ displayName, email, password })),
+});
+export default connect(null, mapDispatchToProps)(SignUp);
---
+++ client/src/pages/collection/collection.component.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+
+import CollectionItem from "../../components/collections-item/collections-item.component";
+import CollectionsContext from "../../contexts/collections/contexts.collections";
+
+import "./collection.styles.scss";
+
+const CollectionPage = ({ match }) => {
+  const collections = useContext(CollectionsContext);
+  const collection = collections[match.params.categoryId];
+  const { title, items } = collection;
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CollectionPage;
